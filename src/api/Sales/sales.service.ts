49753f7d52c3176b759f6d702ee8fb033d6417bd
Alberto Mtz <jalberto.mf21@gmail.com>
2022-11-10 23:47:41 -0700

    Docker Integration

--- src/api/Sales/sales.service.ts
+++ src/api/Sales/sales.service.ts
@@ -42,37 +42,8 @@ export class SalesService {
                 unit_price: details.unit_price
             })
         });            
-        /*for (let index = 0; index < sale.details.length; index++) {
-            
-            details.product = sale.details[index].product
-            details.quantity = sale.details[index].quantity
-            details.unit_price = sale.details[index].unit_price
-            details.id_sales = (new_sele.id)
-
-            console.log(sale.details[index])
-            console.log(details)
-            this.detailsEntity.insert({
-                id_sales: details.id_sales,
-                quantity: details.quantity,
-                product: details.product,
-                unit_price: details.unit_price
-            })
-            
-        }*/
+        
             
         return true;
     }
 }
-/*async create(sale: ISales){
-        const date = new Date();
-        let total = 0
-        sale.details.forEach(item => {
-            total = total + (item.quantity * item.unit_price)
-        })
-
-        return await this.salesEntity.insert({
-            id_user: sale.id_user,
-            date: date,
-            total
-        })
-    }*/
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,15 @@ import { Connection } from './configs/DBConnection';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SalesModule } from './api/Sales/sales.module';
-import { UserModule } from './api/users/user/user.module';
+import { UserModule } from './api/Users/user/user.module';
 import { DetailsModule } from './api/Details/details.module';
+import { ConfigModule } from  '@nestjs/config';
 
 
 @Module({
-  imports: [Connection, UserModule, SalesModule, DetailsModule],
+  imports: [ConfigModule.forRoot({
+    envFilePath: process.env.NODE_ENV === 'docker' ? '.env' : '.env.local'
+  }), Connection, UserModule, SalesModule, DetailsModule],
   controllers: [AppController],
   providers: [AppService],
 })
